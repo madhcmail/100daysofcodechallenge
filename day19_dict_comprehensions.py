2294283943228ee12dcb28f4bfd58499ca303e63
Madhuri Charugundla <57572354+madhcmail@users.noreply.github.com>
2020-05-07 19:19:57 -0700

    update complete dataset

--- day19_dict_comprehensions.py
+++ day19_dict_comprehensions.py
@@ -2,7 +2,20 @@ bites = {6: 'PyBites Die Hard',
          7: 'Parsing dates from logs',
          9: 'Palindromes',
          10: 'Practice exceptions',
-         11: 'Enrich a class with dunder methods'}
-exclude_bites = (6,10)
+         11: 'Enrich a class with dunder methods',
+         12: 'Write a user validation function',
+         13: 'Convert dict in namedtuple/json',
+         14: 'Generate a table of n sequences',
+         15: 'Enumerate 2 sequences',
+         16: 'Special PyBites date generator',
+         17: 'Form teams from a group of friends',
+         18: 'Find the most common word',
+         19: 'Write a simple property',
+         20: 'Write a context manager',
+         21: 'Query a nested data structure'}
+exclude_bites = {6, 10, 16, 18, 21}
 
-print({key:value for (key,value) in bites.items() if key not in exclude_bites})
+
+def filter_bites(bites=bites, bites_done=exclude_bites):
+    """return the bites dict with the exclude_bites filtered out"""
+    return {key:value for (key,value) in bites.items() if key not in exclude_bites}
