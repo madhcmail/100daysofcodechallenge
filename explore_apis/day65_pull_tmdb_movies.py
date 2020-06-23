67d1b500bf2539aebc3b45767c229bbf1672dd02
Madhuri Charugundla <57572354+madhcmail@users.noreply.github.com>
2020-06-22 23:14:26 -0400

    #day 65 - change key

--- explore_apis/day65_pull_tmdb_movies.py
+++ explore_apis/day65_pull_tmdb_movies.py
@@ -6,7 +6,7 @@ results =[]
 
 # pull the data from multiple pages using pagination and write it to json file.
 for i in range(1,5):
-    URL = f'https://api.themoviedb.org/3/discover/movie?api_key=d01cc7ef60ad4d90f60104617e000d90&page={i}'
+    URL = f'https://api.themoviedb.org/3/discover/movie?api_key=<api_key>&page={i}'
     print("Downloading", URL)
     r = requests.get(URL)
     data = r.json()
