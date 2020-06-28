41830f931c0da8d120f184f9cc656b267d6513ff
Madhuri Charugundla <57572354+madhcmail@users.noreply.github.com>
2020-06-27 20:10:37 -0400

    #day70

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
     {
         'author': 'Madhuri',
-        'title': 'Blog Post',
+        'title': 'Blog Post 1',
         'content': 'First Post Content',
         'date_posted': 'June 26, 2020'
     },
     {
         'author': 'Madhuri',
-        'title': 'Blog Post',
+        'title': 'Blog Post 2',
         'content': 'Second Post Content',
         'date_posted': 'June 26, 2020'
     }
