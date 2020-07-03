7dd100f488482433cd66de774eee7c78fc929b56
Madhuri Charugundla <57572354+madhcmail@users.noreply.github.com>
2020-07-03 15:31:06 -0400

    #day74

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+# secret_key is required for application when creating Forms to protect cookies, cross-site attcks, forgeries
+# generate a random key using secrets module
+app.config['SECRET_KEY'] = '1a700710114e598f9e01932bc41ed313'
 
 posts = [
     {
@@ -16,6 +21,7 @@ posts = [
     }
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -25,6 +31,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in !', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username or password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
--- Flask_Blog/forms.py
+++ Flask_Blog/forms.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=DataRequired())
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('Password')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=DataRequired())
+    password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')  # cookie to remember for sometime even after closing the browser
     submit = SubmitField('Login')
