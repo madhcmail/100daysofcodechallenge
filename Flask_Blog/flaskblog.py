c95a5cebc0f876330ef3272882bffb05738fb623
Madhuri Charugundla <57572354+madhcmail@users.noreply.github.com>
2020-07-15 08:09:02 -0400

    #day85

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,10 +1,40 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
-# secret_key is required for application when creating Forms to protect cookies, cross-site attcks, forgeries
+app = Flask(__name__)
+# secret_key is required for application when creating Forms to protect cookies, cross-site attacks, forgeries
 # generate a random key using secrets module
-app.config['SECRET_KEY'] = '1a700710114e598f9e01932bc41ed313'
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default= 'default.jpg')
+    password = db.Column(db.String(60), nullable= False)
+
+    # user and posts have relationship. A user is the author. One user can have multiple posts.But one post has one author.
+    #one to many relationship
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default= datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
 
 posts = [
     {
