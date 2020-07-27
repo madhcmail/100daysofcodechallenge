5fc7941ec365fc44b8b3a4d3ba4bdb0e357e20c3
Madhuri Charugundla <57572354+madhcmail@users.noreply.github.com>
2020-07-27 08:30:40 -0400

    #day94

--- blog_creation/readapi.py
+++ blog_creation/readapi.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from program import db, app, Blogpost
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -12,6 +13,19 @@ def getPosts():
         posts.append({'id':post.id, 'title':post.title, 'subtitle':post.sub_title,'author':post.author,
                       'date_posted': post.date_posted,'content': post.content })
     return jsonify({'Posts':posts})
+
+
+@app.route('/addposts', methods=['POST'])
+def addposts():
+
+    new_post = Blogpost(title = request.json['title'],sub_title = request.json['sub_title'],
+                        author = request.json['author'],date_posted = datetime.now() ,
+                        content= request.json['content'])
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return "successful"
 
 
 if __name__ == '__main__':
